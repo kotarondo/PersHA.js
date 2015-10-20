8cd6efc9eb390056dd81d0d6ddb822b47cf95fbd
Kotaro Endo <kotarondo@gmail.com>
2015-10-20 21:38:21 +0900

    modify tests

--- test/v8bench/test.sh
+++ test/v8bench/test.sh
@@ -2,11 +2,14 @@
 
 make download
 
-persha -init << EOF
+cat >test.js <<EOF
 var fs = require('fs');
 var vm = require('vm');
 function load(f){ vm.runInThisContext(fs.readFileSync(f), f); }
 global.print = console.log;
+global.load = load;
 load("run.js");
 process.exit(0);
 EOF
+
+persha -init test.js
